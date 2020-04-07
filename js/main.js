0c17a0a13e19a6e16e12bcdb21aa69282f1a9c9a
Alberto Dragoni <albertodragoni@yahoo.it>
2020-04-07 15:43:22 +0200

    chiamata api

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-alert('ciao');
+
+$.ajax({
+    url: 'http://157.230.17.132:4007/sales',
+    method: 'GET',
+    success: function (data) {
+        for (var i = 0; i < data.length; i++) {
+            var risultato = data[i];
+            var venditori = {
+                id: risultato.id,
+                saleman: risultato.salesman,
+                amount: risultato.amount,
+                date: risultato.date
+            };
+            console.log(venditori);
+        }
+    }
+});
